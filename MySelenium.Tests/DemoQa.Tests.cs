843cce980c8453ad6fdd70bf959cc6922fb412a1
Zlatyo Uzunov <uzunovz@students.softuni.bg>
2017-04-08 13:08:38 +0300

    10 negative tests. First homework submit

--- MySelenium.Tests/DemoQa.Tests.cs
+++ MySelenium.Tests/DemoQa.Tests.cs
@@ -9,7 +9,8 @@
     using Pages.HomePage;
     using Pages.RegistrationPage;
 
-    [TestFixture]
+    [TestFixture, Property("Priority",0)]
+    [Author("Some", "Guy")]
     public class DemoQaGoToRegPage
     {
         private IWebDriver _chromeDriver;
@@ -39,12 +40,16 @@
         }
     }
 
-    [TestFixture]
+    [TestFixture, Property("Priority",1)]
+    [Author("Jhon","Duddle")]
     public class DemoQaGoRegForm
     {
         private IWebDriver _chromeDriver;
         private RegistrationPage _page;
-        private RegistrationUser _user; 
+        private RegistrationUser _user;
+        private const string EmptyFieldMessage = "* This field is required";
+        private const string BadPhoneMessage = "* Minimum 10 Digits starting with Country Code";
+        private const string ShortPasswordMessage = "* Minimum 8 characters required";
 
         private void InitiateUser()
         {
@@ -82,20 +87,21 @@
             _chromeDriver.Quit();
         }
 
-        [Test]
+        [Test,Property("Priority", 0)]
         public void RegistrateWithNegativePhone()
         {
             _user.PhoneNumber = "21346";
             _page.Fill(_user);
 
+            var messageElement = _page.FormErrorPhone.ElementAtOrDefault(0);
             Assert.AreEqual(
-                _page.FormErrorPhone.ElementAt(0).Text,
-                "* Minimum 10 Digits starting with Country Code",
-                "Phone number is in correct format!"
+                messageElement != null ? messageElement.Text : "",
+                actual: BadPhoneMessage,
+                message: "No error message when phone is incorrect!"
                 );
         }
 
-        [Test]
+        [Test, Property("Priority", 1)]
         public void RegistrateWithNegativeMail()
         {
             _user.Email = "badMail.com";
@@ -104,12 +110,12 @@
             var messageElement = _page.FormErrorEmail.ElementAtOrDefault(0);
             Assert.AreEqual(
                 messageElement != null ? messageElement.Text : "",
-                "* Invalid email address",
-                "E-mail address is correct!"
+                actual: "* Invalid email address",
+                message: "No error message when 'Email' is not valid!"
                 );
         }
 
-        [Test]
+        [Test, Property("Priority", 2)]
         public void RegistrateWithMismatchPasswords()
         {
             _user.PasswordConfirm = "notMatching";
@@ -118,12 +124,12 @@
             var messageElement = _page.FormErrorPasswordConfirm.ElementAtOrDefault(0);
             Assert.AreEqual(
                 messageElement != null ? messageElement.Text : "",
-                "* Fields do not match", 
-                "There is a match in Password and Confirm Password fields!"
+                actual: "* Fields do not match", 
+                message: "No error message when passwords do not match!"
                 );
         }
 
-        [Test]
+        [Test, Property("Priority", 3)]
         public void RegistrateWithEmptyUsername()
         {
             _user.UserName = "";
@@ -132,12 +138,12 @@
             var messageElement = _page.FormErrorUserName.ElementAtOrDefault(0);
             Assert.AreEqual(
                 messageElement != null ? messageElement.Text : "", 
-                "* This field is required", 
-                "Username is correct!"
+                actual: EmptyFieldMessage, 
+                message: "No error message when 'User Name' is empty!"
                 );
         }
 
-        [Test]
+        [Test, Property("Priority", 4)]
         public void RegistrateWithoutHobby()
         {
             _user.Hobbies = new List<bool>(){ false, false, false };
@@ -146,9 +152,79 @@
             var messageElement = _page.FormErrorHobbies.ElementAtOrDefault(0);
             Assert.AreEqual(
                 messageElement != null ? messageElement.Text : "",
-                actual: "* This field is required",
-                message: "Hobby is selected!"
+                actual: EmptyFieldMessage,
+                message: "No error message when field 'Hobby' is empty!"
             );
+        }
+
+        [Test, Property("Priority", 5)]
+        public void RegistrateWithoutLastName()
+        {
+            _user.LastName = "";
+            _page.Fill(_user);
+
+            var messageElement = _page.FormErrorFirstLastName.ElementAtOrDefault(0);
+            Assert.AreEqual(
+                messageElement != null ? messageElement.Text : "",
+                actual: EmptyFieldMessage,
+                message: "No error message when field 'Last Name' is empty!"
+            );
+        }
+
+        [Test, Property("Priority", 6)]
+        public void RegistrateWithNoPhone()
+        {
+            _user.PhoneNumber = "";
+            _page.Fill(_user);
+
+            var messageElement = _page.FormErrorPhone.ElementAtOrDefault(0);
+            Assert.AreEqual(
+                messageElement != null ? messageElement.Text : "",
+                actual: EmptyFieldMessage,
+                message: "No error message when field 'Phone' is empty!"
+                );
+        }
+
+        [Test, Property("Priority", 7)]
+        public void RegistrateWithNotDigitsPhone()
+        {
+            _user.PhoneNumber = "aaaaaaaaaaa";
+            _page.Fill(_user);
+
+            var messageElement = _page.FormErrorPhone.ElementAtOrDefault(0);
+            Assert.AreEqual(
+                messageElement != null ? messageElement.Text : "",
+                actual: BadPhoneMessage,
+                message: "No error message when field 'Phone' does not contain digits!"
+                );
+        }
+
+        [Test, Property("Priority", 8)]
+        public void RegistrateWithDigitsAndCharPhone()
+        {
+            _user.PhoneNumber = "+35999999999";
+            _page.Fill(_user);
+
+            var messageElement = _page.FormErrorPhone.ElementAtOrDefault(0);
+            Assert.AreEqual(
+                messageElement != null ? messageElement.Text : "",
+                actual: BadPhoneMessage,
+                message: "No error message when field 'Phone' is not only digits!"
+                );
+        }
+
+        [Test, Property("Priority", 9)]
+        public void RegistrateWithshortPassword()
+        {
+            _user.Password = "1";
+            _page.Fill(_user);
+
+            var messageElement = _page.FormErrorPassword.ElementAtOrDefault(0);
+            Assert.AreEqual(
+                messageElement != null ? messageElement.Text : "",
+                actual: ShortPasswordMessage,
+                message: "No error message when field 'Password' is less than 8 chars!"
+                );
         }
     }
 }
